e59c8320240bb7a6687dabb936554b7087062d5b
Shivam <bailwalshivam5@gmail.com>
2025-08-11 20:32:37 +0530

    fix loader at App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -139,6 +139,7 @@ function App() {
     return (
       <div style={{
         minHeight: '100vh',
+        minWidth: '100vw',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
